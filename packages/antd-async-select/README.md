0b1175e4e2b817c3a213de95bc015cb5c98c4da2
Seonghyeon Kim <impressor7@gmail.com>
2020-07-30 16:14:12 +0900

    antd-async-select - Add skeleton

---
+++ packages/antd-async-select/README.md
@@ -0,0 +1,3 @@
+# `@tboard-ui/antd-async-select`
+
+> antd select component which can handle async data fetching
---
+++ packages/antd-async-select/lib/antd-async-select.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = antdAsyncSelect;
+
+function antdAsyncSelect() {
+    // TODO
+}
