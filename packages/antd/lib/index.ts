7ac6e9864ef35f168c7af7c2e335cc5f3e20ea76
Seonghyeon Kim <impressor7@gmail.com>
2020-11-10 15:29:10 +0900

    antd - Add AntdButtonGroup to antd package

--- packages/antd/lib/index.ts
+++ packages/antd/lib/index.ts
@@ -1,2 +1,3 @@
 export { default as AntdAsyncSelect } from '@tboard-ui/antd-async-select';
 export { default as AntdInputNumber } from '@tboard-ui/antd-input-number';
+export { default as AntdButtonGroup } from '@tboard-ui/antd-button-group';
